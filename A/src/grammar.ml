2e3b8787b2baa56e3bb0a938df87456a7a551694
Lev Dovzhik <lev.dovzhik@corp.vk.com>
2018-11-18 22:34:10 +0300

    Better interface

--- A/src/grammar.ml
+++ A/src/grammar.ml
@@ -4,22 +4,30 @@ open Buffer;;
 type variable = Name of string
 type expression = Var of variable | Apl of expression * expression | Lambda of variable * expression
 
-let string_of_variable var buff = match var with
+let string_of_variable_impl var buff = match var with
   | Name (s)  -> add_string buff s
+;;
 
-let rec string_of_expression expr buff = match expr with
-	| Var (var)       ->  string_of_variable var buff
+let rec string_of_expression_impl expr buff = match expr with
+	| Var (var)       ->  string_of_variable_impl var buff
 	| Apl (f, g)      ->  begin
                           add_string buff "(";
-                          string_of_expression f buff;
+                          string_of_expression_impl f buff;
                           add_string buff " ";
-                          string_of_expression g buff;
+                          string_of_expression_impl g buff;
                           add_string buff ")";
                         end
 	| Lambda (var, f) ->  begin
                           add_string buff "(\\";
-                          string_of_variable var buff;   
+                          string_of_variable_impl var buff;   
                           add_string buff ".";
-                          string_of_expression f buff;
+                          string_of_expression_impl f buff;
                           add_string buff ")";
-                        end 
+                        end
+;;
+
+let string_of_expression expr = begin
+  let buff = create 100 in
+  string_of_expression_impl expr buff;
+  contents buff
+end;;
--- A/src/main.ml
+++ A/src/main.ml
@@ -4,30 +4,25 @@ open Printf;;
 
 let (>>) x f = f x;;
 
-let rec read_stdin buff = 
+let rec read_stdin_impl buff = 
   try
     begin
     let line = input_line stdin in
     add_string buff line;
     add_string buff "\n";
-    read_stdin buff;
+    read_stdin_impl buff;
     end
   with
-    |End_of_file -> add_string buff ""
+    | End_of_file -> add_string buff ""
 ;;
 
+let read_stdin () = begin
+  let buff = create 100 in
+  read_stdin_impl buff;
+  contents buff
+end;;
 
-let in_buff = create 100;;
-let out_buff = create 100;;
-read_stdin in_buff;;
-let lexed = Lexing.from_string (contents in_buff);;
-let parsed = Parser.main Lexer.main lexed;;
-string_of_expression parsed out_buff;;
-let res = contents out_buff;;
-fprintf stdout "%s\n" res;;
-(*
 read_stdin () >> Lexing.from_string >> Parser.main Lexer.main >> string_of_expression >> fprintf stdout "%s\n";;
-*)
 
 close_out stdout;;
 close_in stdin;;
