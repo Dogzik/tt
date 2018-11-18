2e53277377c3b0c6c0653f5e63c7bc5f59b808a5
Lev Dovzhik <lev.dovzhik@corp.vk.com>
2018-11-18 19:44:12 +0300

    Fix TL

--- A/src/grammar.ml
+++ A/src/grammar.ml
@@ -1,10 +1,25 @@
+open Buffer;;
+
+
 type variable = Name of string
 type expression = Var of variable | Apl of expression * expression | Lambda of variable * expression
 
-let string_of_variable = function
-  | Name (s)  -> s
+let string_of_variable var buff = match var with
+  | Name (s)  -> add_string buff s
 
-let rec string_of_expression = function
-	| Var (var)           -> string_of_variable var
-	| Apl (f, g)          -> "(" ^ (string_of_expression f) ^ " " ^ (string_of_expression g) ^ ")"
-	| Lambda (var, f) -> "(\\" ^ (string_of_variable var) ^ "." ^ (string_of_expression f) ^ ")" 
+let rec string_of_expression expr buff = match expr with
+	| Var (var)       ->  string_of_variable var buff
+	| Apl (f, g)      ->  begin
+                          add_string buff "(";
+                          string_of_expression f buff;
+                          add_string buff " ";
+                          string_of_expression g buff;
+                          add_string buff ")";
+                        end
+	| Lambda (var, f) ->  begin
+                          add_string buff "(\\";
+                          string_of_variable var buff;   
+                          add_string buff ".";
+                          string_of_expression f buff;
+                          add_string buff ")";
+                        end 
--- A/src/main.ml
+++ A/src/main.ml
@@ -4,20 +4,26 @@ open Printf;;
 
 let (>>) x f = f x;;
 
-let rec read_stdin () = 
+let rec read_stdin buff = 
   try
     begin
     let line = input_line stdin in
-    line ^ "\n" ^ read_stdin ()
+    add_string buff line;
+    add_string buff "\n";
+    read_stdin buff;
     end
   with
-    |End_of_file -> "";;
+    |End_of_file -> add_string buff ""
+;;
 
 
-let input = read_stdin ();;
-let lexed = Lexing.from_string input;;
+let in_buff = create 100;;
+let out_buff = create 100;;
+read_stdin in_buff;;
+let lexed = Lexing.from_string (contents in_buff);;
 let parsed = Parser.main Lexer.main lexed;;
-let res = string_of_expression parsed;;
+string_of_expression parsed out_buff;;
+let res = contents out_buff;;
 fprintf stdout "%s\n" res;;
 (*
 read_stdin () >> Lexing.from_string >> Parser.main Lexer.main >> string_of_expression >> fprintf stdout "%s\n";;
